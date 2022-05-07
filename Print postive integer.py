2a017d4950fdbda2d526fc2d026f61d1d8c77d4a
Swetha-bala <103267956+Swetha-bala@users.noreply.github.com>
2022-05-07 14:11:05 +0530

    Print postive integer.py

--- Print postive integer.py
+++ Print postive integer.py
@@ -1,8 +1,21 @@
-list1=[12,-7,5,64,-14]
-for num in list1:
-    if num >= 0:
-     print (num, end = " ")
-list2=[12,14,-95,3]
-for num in list2:
-     if num >= 0:
-      print (num, end = " ")
+li1=[]
+n=int(input("Enter size of list1 "))
+for i in range(0,n):
+    e=int(input("Enter element of list1 "))
+    li1.append(e)
+
+print("Positive numbers in",li1,"are: ")
+for i in li1:   
+    if i >= 0:
+       print(i, end = " ")
+       
+  
+li2=[]
+n=int(input("Enter size of list2 "))
+for i in range(0,n):
+    e=int(input("Enter element of list2 "))
+    li2.append(e)
+print("Positive numbers in",li2,"are: ")
+for i in li2:   
+    if i >= 0:
+       print(i, end = " ")
